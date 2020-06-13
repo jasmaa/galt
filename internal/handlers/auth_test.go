0c03bd9c62025c3ebf8d268a880314e3489db4b4
jasmaa <jasonmaa3955@gmail.com>
2020-06-13 13:42:12 -0400

    Move router setup for test

--- internal/handlers/auth_test.go
+++ internal/handlers/auth_test.go
@@ -8,47 +8,10 @@ import (
 	"testing"
 
 	sqlmock "github.com/DATA-DOG/go-sqlmock"
-	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 
-	"github.com/jasmaa/galt/internal/handlers"
-	"github.com/jasmaa/galt/internal/middleware"
 	"github.com/jasmaa/galt/internal/store"
 )
-
-// TODO: figure out where to put this
-func setupRouter(s store.Store) *gin.Engine {
-
-	gin.SetMode(gin.ReleaseMode)
-
-	hmacSecret := "secret_key"
-
-	// Router
-	r := gin.New()
-
-	// Handlers
-	v1 := r.Group("/api/v1")
-
-	v1.Use(func(c *gin.Context) {
-		c.Set("store", s)
-		c.Next()
-	})
-
-	v1.POST("/createAccount", handlers.CreateAccount())
-	v1.POST("/login", handlers.Login(hmacSecret))
-
-	v1.GET("/user/:userID", handlers.GetUser())
-	v1.GET("/user", middleware.AuthUser(hmacSecret), handlers.GetProfile())
-	v1.PUT("/user", middleware.AuthUser(hmacSecret), handlers.UpdateProfile())
-	v1.DELETE("/user", middleware.AuthUser(hmacSecret), handlers.DeleteProfile())
-
-	v1.GET("/status/:statusID", handlers.GetStatus())
-	v1.POST("/status", middleware.AuthUser(hmacSecret), handlers.PostStatus())
-	v1.PUT("/status/:statusID", middleware.AuthUser(hmacSecret), handlers.UpdateStatus())
-	v1.DELETE("/status/:statusID", middleware.AuthUser(hmacSecret), handlers.DeleteStatus())
-
-	return r
-}
 
 func TestCreateAccountSuccess(t *testing.T) {
 
