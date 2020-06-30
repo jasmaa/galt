5796f1b0d1abfdf2ec0911b9d6c096f1c7786d45
jasmaa <jasonmaa3955@gmail.com>
2020-06-30 18:56:55 -0400

    Use UUID for ids in schema

--- db/init.sql
+++ db/init.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE galt;
 
 CREATE TABLE users (
-    id VARCHAR(255) PRIMARY KEY NOT NULL,
+    id UUID PRIMARY KEY NOT NULL,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     description VARCHAR(255) NOT NULL,
@@ -9,8 +9,8 @@ CREATE TABLE users (
 );
 
 CREATE TABLE statuses (
-    id VARCHAR(255) PRIMARY KEY NOT NULL,
-    user_id VARCHAR(255) NOT NULL,
+    id UUID PRIMARY KEY NOT NULL,
+    user_id UUID NOT NULL,
     content VARCHAR(255) NOT NULL,
     posted_timestamp TIMESTAMP NOT NULL,
     is_edited BOOLEAN NOT NULL,
@@ -18,10 +18,10 @@ CREATE TABLE statuses (
 );
 
 CREATE TABLE comments (
-    id VARCHAR(255) PRIMARY KEY NOT NULL,
-    user_id VARCHAR(255) NOT NULL,
-    status_id VARCHAR(255) NOT NULL,
-    parent_comment_id VARCHAR(255),
+    id UUID PRIMARY KEY NOT NULL,
+    user_id UUID NOT NULL,
+    status_id UUID NOT NULL,
+    parent_comment_id UUID,
     content VARCHAR(255) NOT NULL, 
     posted_timestamp TIMESTAMP NOT NULL,
     is_edited BOOLEAN NOT NULL,
@@ -31,30 +31,30 @@ CREATE TABLE comments (
 );
 
 CREATE TABLE circles (
-    id VARCHAR(255) PRIMARY KEY NOT NULL,
-    user_id VARCHAR(255) NOT NULL,
+    id UUID PRIMARY KEY NOT NULL,
+    user_id UUID NOT NULL,
     name VARCHAR(255) NOT NULL,
     description VARCHAR(255) NOT NULL,
     FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE circle_user_pairs (
-    user_id VARCHAR(255) NOT NULL,
-    circle_id VARCHAR(255) NOT NULL,
+    user_id UUID NOT NULL,
+    circle_id UUID NOT NULL,
     FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY(circle_id) REFERENCES circles(id) ON DELETE CASCADE
 );
 
 CREATE TABLE status_like_pairs (
-    user_id VARCHAR(255) NOT NULL,
-    status_id VARCHAR(255) NOT NULL,
+    user_id UUID NOT NULL,
+    status_id UUID NOT NULL,
     FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY(status_id) REFERENCES statuses(id) ON DELETE CASCADE
 );
 
 CREATE TABLE comment_like_pairs (
-    user_id VARCHAR(255) NOT NULL,
-    comment_id VARCHAR(255) NOT NULL,
+    user_id UUID NOT NULL,
+    comment_id UUID NOT NULL,
     FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY(comment_id) REFERENCES comments(id) ON DELETE CASCADE
 );
